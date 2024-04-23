b1437a2cc92c3ca6c3a435eff094b79829cab0d3
LuisNettoo <santosneto.luis@gmail.com>
2024-04-23 08:39:16 -0300

    Tentativa de resposividade

--- src/components/CardProject/styles.ts
+++ src/components/CardProject/styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const CardContainer = styled.div`
-  max-width: 384px;
+  max-width: 28%;
   height: 344px;
   padding: 1.5rem;
   background-color: var(--surface-primary);
