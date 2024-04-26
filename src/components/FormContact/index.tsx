4b25b81aea41a702f92bc94e3b8bb2e1f2a7f18d
LuisNettoo <santosneto.luis@gmail.com>
2024-04-26 10:51:50 -0300

    limpeza de campos configurada

--- src/components/FormContact/index.tsx
+++ src/components/FormContact/index.tsx
@@ -1,43 +1,56 @@
-import { FormEventHandler, useRef } from "react"
-import emailjs from "@emailjs/browser"
-
-import { Container } from "./styles"
-
-import profileImg from "../../assets/profile.svg"
-import arrowRightIcon from "../../assets/arrow-right.svg"
-import logoGithubIcon from "../../assets/logo-github.svg"
-import logoLinkedinIcon from "../../assets/logo-linkedin.svg"
-import logoInstagramIcon from "../../assets/logo-instagram.svg"
-
+import { FormEventHandler, useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import { Container } from "./styles";
+import profileImg from "../../assets/profile.svg";
+import arrowRightIcon from "../../assets/arrow-right.svg";
+import logoGithubIcon from "../../assets/logo-github.svg";
+import logoLinkedinIcon from "../../assets/logo-linkedin.svg";
+import logoInstagramIcon from "../../assets/logo-instagram.svg";
 
 function FormContact() {
-  const form = useRef(new HTMLFormElement)
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [message, setMessage] = useState("")
+  const form = useRef(null)
 
-  const sendEmail: FormEventHandler<HTMLFormElement> = (event) => {
-    event.preventDefault()
+  const sendEmail = (event: React.FormEvent) => {
+    event.preventDefault();
 
-    emailjs
-      .sendForm("service_0utbtuk", "template_ryl44yh", form.current, {
-        publicKey: "cMacAY6mylxJ4KLrP",
-      })
-      .then(() => {
-        console.log("Mensagem enviada!")
-      },
-      (error: any) => {
-        console.log("Ocorreu uma falha...", error.text)
-      })
-      
-  }
+    // Check if form.current is not null before using it
+    if (form.current) {
+      emailjs
+        .sendForm("service_0utbtuk", "template_ryl44yh", form.current, {
+          publicKey: "cMacAY6mylxJ4KLrP",
+        })
+        .then(
+          () => {
+            console.log("Mensagem enviada!");
+          },
+          (error) => {
+            console.log("Ocorreu uma falha...", error.text);
+          }
+        );
+
+        setName("")
+        setEmail("")
+        setMessage("")
+
+        
+    } else {
+      console.log("Form reference is null");
+    }
+  };
 
   return (
     <Container>
       <section>
         <img src={profileImg} alt="" />
-
         <h3>Contato</h3>
         <h2>Gostou do meu trabalho? Vamos trabalhar juntos</h2>
-        <p>Eu estou sempre disposto no chat. Mande um email para lmss.neto@discente.ufma.br ou fale comigo por alguma rede social.</p>
-
+        <p>
+          Eu estou sempre disposto no chat. Mande um email para
+          lmss.neto@discente.ufma.br ou fale comigo por alguma rede social.
+        </p>
         <div>
           <a href="https://github.com/LuisNettoo" target="_blank">
             <img src={logoGithubIcon} alt="Logo do Github" />
@@ -51,16 +64,37 @@ function FormContact() {
         </div>
       </section>
       <form ref={form} onSubmit={sendEmail}>
-        <input type="text" placeholder="Nome" required name="user_name"/>
-        <input type="email" placeholder="E-mail" required name="user_email"/>
-        <textarea cols={30} rows={10} placeholder="Sua mensagem" required name="message"/>
-      
+        <input 
+          type="text" 
+          placeholder="Nome" 
+          required 
+          name="user_name" 
+          value={name}
+          onChange={e => setName(e.target.value)}  
+        />
+        <input 
+          type="email" 
+          placeholder="E-mail" 
+          required 
+          name="user_email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}   
+        />
+        <textarea
+          cols={30}
+          rows={10}
+          placeholder="Sua mensagem"
+          required
+          name="message"
+          value={message}
+          onChange={e => setMessage(e.target.value)}
+        ></textarea>
         <button type="submit">
           Envie sua mensagem <img src={arrowRightIcon} alt="Seta para a direita" />
         </button>
       </form>
     </Container>
-  )
+  );
 }
 
-export default FormContact
+export default FormContact;
